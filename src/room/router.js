b5649a77b77d72c4268ec5882df5a42d31c33396
learnerdilip <diliptalks@gmail.com>
2020-02-18 10:23:15 +0100

    added route for getting all rooms

--- src/room/router.js
+++ src/room/router.js
@@ -5,12 +5,20 @@ const auth = require("../auth/middleWare");
 
 const router = new Router();
 
-router.post("/game", auth, async (request, response, next) => {
+router.post("/room", auth, async (request, response, next) => {
   try {
     console.log("THE REQ BODY:", request.body);
     const roomCreate = { room_name: request.body.room };
-    console.log("THE CREATED ROOM:", roomCreate);
+    // console.log("THE CREATED ROOM:", roomCreate);
     Room.create(roomCreate).then(room => response.send(room));
+  } catch {
+    error => next(error);
+  }
+});
+
+router.get("/rooms", (request, response, next) => {
+  try {
+    Room.findAll({ include: [User] }).then(rooms => response.send(rooms));
   } catch {
     error => next(error);
   }
