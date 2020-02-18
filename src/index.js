9f04e09c811013f6a541cd4a9ca1c82960594445
torkvell <tor.kvell@outlook.com>
2020-02-18 14:32:53 +0100

    added stream for lobby

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const Sse = require("json-sse");
+const roomFactory = require("./room/router");
+const Room = require("./room/model");
+const User = require("./user/model");
+
+const stream = new Sse();
 
 const userRouter = require("./user/router");
-const roomRouter = require("./room/router");
+// const roomRouter = require("./room/router");
 
 const port = process.env.PORT || 4000;
 const cors = require("cors");
@@ -11,11 +17,23 @@ const corsMiddleware = cors();
 app.use(corsMiddleware);
 app.use(express.json()); //Express own bodyParser
 
-app.use("/lobby", roomRouter);
+// app.use("/lobby", roomRouter);
 app.use("/user", userRouter); //Pass the router to app.use to register the middleware.
 app.get("/test", (req, res, next) => {
   res.send("Test endpoint working");
 });
-//Pass the port and a logging function to app.listen to start the server.
 
+app.get("/stream", async (req, res, next) => {
+  try {
+    const rooms = await Room.findAll({ include: [User] });
+    console.log(rooms);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const roomRouter = roomFactory(stream);
+app.use(roomRouter);
+
+//Pass the port and a logging function to app.listen to start the server.
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/room/router.js
+++ src/room/router.js
@@ -3,40 +3,50 @@ const Room = require("./model");
 const User = require("../user/model");
 const auth = require("../auth/middleWare");
 
-const router = new Router();
+const factory = stream => {
+  const router = new Router();
 
-router.post("/room", auth, async (request, response, next) => {
-  try {
-    console.log("THE REQ BODY:", request.body);
-    const roomCreate = { room_name: request.body.room };
-    // console.log("THE CREATED ROOM:", roomCreate);
-    Room.create(roomCreate).then(room => response.send(room));
-  } catch {
-    error => next(error);
-  }
-});
+  router.post("/room", async (request, response, next) => {
+    try {
+      // console.log("THE REQ BODY:", request.body);
+      const roomCreate = { room_name: request.body.room_name };
+      const ref = await Room.create(roomCreate);
+      const room = await Room.findByPk(ref.id, { include: [User] });
+      const action = {
+        type: "ROOM_CREATED",
+        payload: room
+      };
+      const json = JSON.stringify(action);
+      stream.send(json);
+      // response.send(json)
+    } catch {
+      error => next(error);
+    }
+  });
 
-router.get("/rooms", (request, response, next) => {
-  try {
-    Room.findAll({ include: [User] }).then(rooms => response.send(rooms));
-  } catch {
-    error => next(error);
-  }
-});
+  router.get("/rooms", (request, response, next) => {
+    try {
+      Room.findAll({ include: [User] }).then(rooms => response.send(rooms));
+    } catch {
+      error => next(error);
+    }
+  });
 
-router.delete("/room", (request, response, next) => {
-  Room.findByPk(request.body.id)
-    .then(room =>
-      Room.destroy({
-        where: {
-          id: room.id
-        }
-      })
-    )
-    .then(room => {
-      // console.log("ROOM IN BACKEND", request.body.id);
-      response.json(request.body.id);
-    });
-});
+  router.delete("/room", (request, response, next) => {
+    Room.findByPk(request.body.id)
+      .then(room =>
+        Room.destroy({
+          where: {
+            id: room.id
+          }
+        })
+      )
+      .then(room => {
+        // console.log("ROOM IN BACKEND", request.body.id);
+        response.json(request.body.id);
+      });
+  });
+  return router;
+};
 
-module.exports = router;
+module.exports = factory;
