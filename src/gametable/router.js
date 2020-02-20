e730b133fb37e6b9cd796e0e6864e4a8611d1f4a
torkvell <tor.kvell@outlook.com>
2020-02-20 10:39:02 +0100

    server broken

--- src/gametable/router.js
+++ src/gametable/router.js
@@ -7,10 +7,13 @@ function factory(stream) {
   const router = new Router();
 
   router.post("/addGameToUser", (req, res, next) => {
+    console.log("ADD GAME TO USER ENDPOINT", req);
     try {
       const gameid = req.body.gameId;
       const userid = req.body.userId;
+      console.log("GAME ID AND USER ID", gameid, userid);
       User.findByPk(userid).then(user => {
+        console.log("USER------------------", user);
         user.roomId = gameid;
         user.save();
         res.json(user.roomId);
@@ -21,6 +24,10 @@ function factory(stream) {
   });
 
   router.post("/gametable", async (req, res, next) => {
+    console.log(
+      `REQUEST FROM CLIENT ------------------------->`,
+      req.body.data
+    );
     card_deck
       .findOne({
         where: {
@@ -28,7 +35,9 @@ function factory(stream) {
         }
       })
       .then(response => {
-        console.log("response:", response);
+        console.log(
+          `Find card deck where room id=${req.body.data}. DeckID for this room: ${response.dataValues.deck_id}`
+        );
         const deck_id = response.dataValues.deck_id;
         User.findAll({
           where: {
@@ -44,10 +53,9 @@ function factory(stream) {
             type: "TABLE_USERS",
             payload: data
           };
-          // console.log("DATA---------------------", data);
+          console.log("DATA---------------------", action);
           const json = JSON.stringify(action);
           stream.send(json);
-          // res.send(data);
         });
       });
 
